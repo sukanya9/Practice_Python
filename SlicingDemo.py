6e63ca4bb0bc47b04a6182512ec354407e5371b7
Sukanya Pasupula <sukanya.pasupula9@gmail.com>
2026-09-03 23:00:10 +0530

    Update SlicingDemo.py
    Updated the  commit related to slicing

--- SlicingDemo.py
+++ SlicingDemo.py
@@ -8,3 +8,9 @@ print(name[10:2])
 print(name[::-1]) # reverse the string
 sent="Python"
 print("Reverse String: " + sent[::-1]) # reverse the string
+
+print("------------------")
+print(name[:7]) # from start to 6th index
+print(name[7:10]) # from 7th index to 10th index
+print(name[7:]) # from 7th index to 10th index with a step of 2
+print(name[7:10:1]) # from 7th index to 10th index with a step of 1
